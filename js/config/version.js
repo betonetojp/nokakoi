b4540823c92160decd4d0d40853b2c292b5371b6
betoneto <109812892+betonetojp@users.noreply.github.com>
2026-08-11 08:14:52 +0900

    feat(release): バージョンを1.121.2に更新し、初期リレー追加

--- js/config/version.js
+++ js/config/version.js
@@ -1,1 +1,1 @@
-export const VERSION = '1.121.1';
+export const VERSION = '1.121.2';
--- js/core/relay/relay-constants.js
+++ js/core/relay/relay-constants.js
@@ -2,11 +2,12 @@
  * デフォルトのリレーリスト（読込/書込フラグ付き）
  */
 export const defaultIntlRelayUrl = 'wss://nos.lol';
-export const defaultJaRelayUrl = 'wss://yabu.me';
+export const defaultJaRelayUrl = 'wss://relay-jp.nostr.wirednet.jp';
 
 export const defaultRelays = [
   { url: defaultIntlRelayUrl, read: true, write: true },
-  { url: defaultJaRelayUrl, read: true, write: true }
+  { url: defaultJaRelayUrl, read: true, write: true },
+  { url: 'wss://yabu.me', read: true, write: true }
 ];
 
 export const profileIndexerRelay = 'wss://directory.yabu.me';
